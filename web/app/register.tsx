e252648d893a8c47276c988732e8e220fd1794e1
Ksawery Zietara <Ksawery.Zietara@sap.com>
2024-09-18 16:17:22 +0200

    Create register and login page

--- web/app/register.tsx
+++ web/app/register.tsx
@@ -1,19 +1,31 @@
 import React, {useState} from "react";
-import {StatusBar, Text, TextInput, View} from "react-native";
+import {View, Text, StatusBar, TextInput} from "react-native";
 import CustomButton from "@/components/CustomButton";
 import {useRouter} from "expo-router";
 import axios from "axios";
-import {saveJWT} from "@/utils/auth";
 
-const LoginScreen = () => {
+const RegisterScreen = () => {
     const router = useRouter();
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
+    const [confirmPassword, setConfirmPassword] = useState("");
     const [errorMessage, setErrorMessage] = useState("");
 
     const validateFields = () => {
-        if (!email.trim() || !password) {
+        if (
+            !email.trim() ||
+            !password ||
+            !confirmPassword
+        ) {
             return "Wszystkie pola muszą być wypełnione.";
+        }
+
+        if (email.length > 255 || password.length > 255 || confirmPassword.length > 255) {
+            return "Imię, Nazwisko i Email nie mogą przekraczać 40 znaków.";
+        }
+
+        if (password.length > 60) {
+            return "Hasło nie może przekraczać 60 znaków.";
         }
 
         const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
@@ -21,10 +33,19 @@ const LoginScreen = () => {
             return "Nieprawidłowy format adresu e-mail.";
         }
 
+        const passwordRegex = /^(?=.*[A-Z])(?=.*\d)[A-Za-z\d]{8,60}$/;
+        if (!passwordRegex.test(password)) {
+            return "Hasło musi mieć co najmniej 8 znaków, zawierać co najmniej jedną wielką literę i jedną cyfrę.";
+        }
+
+        if (password !== confirmPassword) {
+            return "Hasła muszą być takie same.";
+        }
+
         return null;
     };
 
-    const handleLogin = async () => {
+    const handleRegister = async () => {
         const validationError = validateFields();
 
         if (validationError) {
@@ -32,22 +53,17 @@ const LoginScreen = () => {
             return;
         }
 
-        await axios
-            .post("/api/customer/login", {
-                email,
-                password,
-            })
+        await axios.post('/api/customer/register', {
+            email,
+            password,
+            confirmPassword
+        })
             .then(function (response) {
                 setErrorMessage("");
-                saveJWT(response.data.jwt);
-                router.push("/home");
+                router.push("/login");
             })
             .catch(function (error) {
-                if (error.response.status === 400) {
-                    setErrorMessage(error.response.data.message);
-                } else {
-                    setErrorMessage("Nieprawidłowy email lub hasło.");
-                }
+                setErrorMessage(error.response.data.message);
             });
     };
 
@@ -58,16 +74,7 @@ const LoginScreen = () => {
             </View>
             <View className="flex-1 justify-center items-center px-6">
                 <View className="w-full max-w-xl">
-                    <Text className="text-center text-3xl font-bold text-white">Logowanie</Text>
-                    <Text className="text-center text-gray-400 mt-4 mb-4">
-                        Jesteś tutaj nowy?{" "}
-                        <Text
-                            className="text-[#ff5c5c] font-bold"
-                            onPress={() => router.push("/register")}
-                        >
-                            Zarejestruj się tutaj
-                        </Text>
-                    </Text>
+                    <Text className="text-center text-3xl font-bold text-white mb-4">Utwórz konto</Text>
                     <TextInput
                         placeholder="Email"
                         keyboardType="email-address"
@@ -84,17 +91,31 @@ const LoginScreen = () => {
                         placeholderTextColor="#aaa"
                         className="bg-[#2d2d2d] text-white rounded-lg pl-4 py-3 mb-4"
                     />
+                    <TextInput
+                        placeholder="Potwierdź hasło"
+                        secureTextEntry
+                        value={confirmPassword}
+                        onChangeText={setConfirmPassword}
+                        placeholderTextColor="#aaa"
+                        className="bg-[#2d2d2d] text-white rounded-lg pl-4 py-3 mb-4"
+                    />
                     {errorMessage && (
                         <Text className="text-[#ff5c5c] text-center mt-2">{errorMessage}</Text>
                     )}
                     <CustomButton
-                        title="Zaloguj się"
-                        onPress={handleLogin}
+                        title="Utwórz konto"
+                        onPress={handleRegister}
                         containerStyles="bg-[#ff5c5c] mt-4 self-center w-3/5"
                         textStyles="text-white font-bold"
                     />
                     <Text className="text-center text-gray-400 mt-4">
-                        Zapomniałeś hasła?
+                        Masz już konto?{" "}
+                        <Text
+                            className="text-[#ff5c5c] font-bold"
+                            onPress={() => router.push("/login")}
+                        >
+                            Zaloguj się tutaj
+                        </Text>
                     </Text>
                 </View>
             </View>
@@ -103,4 +124,4 @@ const LoginScreen = () => {
     );
 };
 
-export default LoginScreen;
+export default RegisterScreen;
